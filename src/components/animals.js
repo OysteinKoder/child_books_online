53fea8655566a57faa591ecdd46fcac3e291a57a
OysteinKoder <oystein.kodehode@gmail.com>
2025-03-04 06:28:52 +0100

    Refactor animal component to streamline audio playback functions and add jiggle animation

--- src/components/animals.js
+++ src/components/animals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BoxNumber from "./boxNumber";
 import { animalData } from "../data/animalData";
 
@@ -20,88 +20,99 @@ function StopPlay() {
 // Add CSS for animations
 const styles = {
   gridCell: {
-    transition: "transform 0.5s ease-in-out",
-    animation: "rotateAnimation 1s infinite alternate",
+    transition: "transform 0.3s ease-in-out",
   },
   gridCellActive: {
-    transform: "scale(1.2) rotate(15deg)",
-    transition: "transform 0.5s ease-in-out",
+    animation: "jiggle 0.3s infinite",
+    transform: "scale(1.2)",
   },
 };
 
+// Add keyframes for jiggle animation
+const addJiggleKeyframes = () => {
+  const styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = `
+    @keyframes jiggle {
+      0%, 100% { transform: translateX(0) scale(1.3); }
+      25% { transform: translateX(-5px) scale(1.3); }
+      75% { transform: translateX(5px) scale(1.3); }
+    }
+  `;
+  document.head.appendChild(styleSheet);
+};
+
 // Functions for playing audio files
-function PlayCat() {
-  StopPlay();
-  animateAnimal("cat");
-  audioElements.cat.play();
-  playstate = !playstate;
-}
-
-function PlayCow() {
-  StopPlay();
-  animateAnimal("cow");
-  audioElements.cow.play();
-  playstate = !playstate;
-}
-
-function PlayDog() {
-  StopPlay();
-  animateAnimal("dog");
-  audioElements.dog.play();
-  playstate = !playstate;
-}
-
-function PlayHippo() {
-  StopPlay();
-  animateAnimal("hippo");
-  audioElements.hippo.play();
-  playstate = !playstate;
-}
-
-function PlayHorse() {
-  StopPlay();
-  animateAnimal("horse");
-  audioElements.horse.play();
-  playstate = !playstate;
-}
-
-function PlayLion() {
-  StopPlay();
-  animateAnimal("lion");
-  audioElements.lion.play();
-  playstate = !playstate;
-}
-
-function PlayRooster() {
-  StopPlay();
-  animateAnimal("chicken");
-  audioElements.chicken.play();
-  playstate = !playstate;
-}
-
-function PlaySheep() {
-  StopPlay();
-  animateAnimal("sheep");
-  audioElements.sheep.play();
-  playstate = !playstate;
-}
-
-function PlayZeebra() {
-  StopPlay();
-  animateAnimal("zebra");
-  audioElements.zebra.play();
-  playstate = !playstate;
-}
+const playFunctions = {
+  cat: () => {
+    StopPlay();
+    animateAnimal("cat");
+    audioElements.cat.play();
+    playstate = !playstate;
+  },
+  cow: () => {
+    StopPlay();
+    animateAnimal("cow");
+    audioElements.cow.play();
+    playstate = !playstate;
+  },
+  dog: () => {
+    StopPlay();
+    animateAnimal("dog");
+    audioElements.dog.play();
+    playstate = !playstate;
+  },
+  hippo: () => {
+    StopPlay();
+    animateAnimal("hippo");
+    audioElements.hippo.play();
+    playstate = !playstate;
+  },
+  horse: () => {
+    StopPlay();
+    animateAnimal("horse");
+    audioElements.horse.play();
+    playstate = !playstate;
+  },
+  lion: () => {
+    StopPlay();
+    animateAnimal("lion");
+    audioElements.lion.play();
+    playstate = !playstate;
+  },
+  chicken: () => {
+    StopPlay();
+    animateAnimal("chicken");
+    audioElements.chicken.play();
+    playstate = !playstate;
+  },
+  sheep: () => {
+    StopPlay();
+    animateAnimal("sheep");
+    audioElements.sheep.play();
+    playstate = !playstate;
+  },
+  zebra: () => {
+    StopPlay();
+    animateAnimal("zebra");
+    audioElements.zebra.play();
+    playstate = !playstate;
+  },
+};
 
 function animateAnimal(animal) {
   const img = document.querySelector(`img[alt="${animal}"]`);
-  img.style.transform = "scale(1.2) rotate(15deg)";
+  img.style.animation = "jiggle 0.5s infinite";
   setTimeout(() => {
-    img.style.transform = "scale(1) rotate(0deg)";
-  }, audioElements[animal].duration * 1000 + 500); // Adjust duration based on audio length
+    img.style.animation = "";
+  }, audioElements[animal].duration * 1000); // Adjust duration based on audio length
 }
 
 function Animals() {
+  useEffect(() => {
+    addJiggleKeyframes();
+  }, []);
+
   return (
     <div className="Center">
       <div className="Text-box">
@@ -112,69 +123,16 @@ function Animals() {
       </div>
       <BoxNumber text="Box One"></BoxNumber>
       <div className="Grid">
-        <img
-          className="Grid-cell"
-          src={animalData.cat.image}
-          alt="cat"
-          onClick={PlayCat}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.chicken.image}
-          alt="chicken"
-          onClick={PlayRooster}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.cow.image}
-          alt="cow"
-          onClick={PlayCow}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.dog.image}
-          alt="dog"
-          onClick={PlayDog}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.hippo.image}
-          alt="hippo"
-          onClick={PlayHippo}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.horse.image}
-          alt="horse"
-          onClick={PlayHorse}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.lion.image}
-          alt="lion"
-          onClick={PlayLion}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.sheep.image}
-          alt="sheep"
-          onClick={PlaySheep}
-          style={styles.gridCell}
-        ></img>
-        <img
-          className="Grid-cell"
-          src={animalData.zebra.image}
-          alt="zebra"
-          onClick={PlayZeebra}
-          style={styles.gridCell}
-        ></img>
+        {Object.keys(animalData).map((animal) => (
+          <img
+            key={animal}
+            className="Grid-cell"
+            src={animalData[animal].image}
+            alt={animal}
+            onClick={playFunctions[animal]}
+            style={styles.gridCell}
+          ></img>
+        ))}
       </div>
     </div>
   );
