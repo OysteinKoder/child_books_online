b1b6aa13612e0d1df55fcb253e5ecc6ec13c88ae
OysteinKoder <oystein.kodehode@gmail.com>
2025-03-04 07:00:48 +0100

    Remove unused image and audio imports in items component to improve code cleanliness and maintainability

--- src/components/items.js
+++ src/components/items.js
@@ -1,28 +1,8 @@
 import React, { useEffect } from "react";
-import Hammer from "../img/items/hammer.jpg";
-import Bus from "../img/items/bus.jpg";
-import Car from "../img/items/car.jpg";
-import Chair from "../img/items/chair.jpg";
-import Helicopter from "../img/items/helicopter.jpg";
-import Pants from "../img/items/pants.jpg";
-import Plane from "../img/items/plane.jpg";
-import Shoes from "../img/items/shoes.jpg";
-import tShirt from "../img/items/tShirt.jpg";
 import BoxNumber from "./boxNumber";
 import { itemData } from "../data/itemData";
 
 // audio imports
-
-import BusVoice from "../audio/items/bus.mp3";
-import CarVoice from "../audio/items/car.mp3";
-import ChairVoice from "../audio/items/chair.mp3";
-import HammerVoice from "../audio/items/hammer.mp3";
-import HelicopterVoice from "../audio/items/helicopter.mp3";
-import PantsVoice from "../audio/items/pants.mp3";
-import PlaneVoice from "../audio/items/plane.mp3";
-import ShoesVoice from "../audio/items/shoes.mp3";
-import TshirtVoice from "../audio/items/tshirt.mp3";
-
 const audioElements = Object.keys(itemData).reduce((acc, key) => {
   acc[key] = new Audio(itemData[key].audio);
   return acc;
